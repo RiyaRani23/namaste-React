9da76738fd4bdcf6cdee826b134ccfe76ce72b7c
RiyaRani23 <riyarani1404@gmail.com>
2025-12-23 22:23:17 +0530

    JSX

--- 03_LayingFoundation/app.js
+++ 03_LayingFoundation/app.js
@@ -1,2 +1,10 @@
 import React from "react";
-import ReactDOM from "react_dom/client";
+import ReactDOM from "react-dom/client";
+
+// JSX (transpiles before it reaches the JS engine) - parcel - babel
+
+// JSX -> React.createElement() -> JS Object -> HTML (DOM)
+const jsxHeading = <h1 id="title" className="headClass" key="h1tag">Hello from JSX Heading!</h1>;
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(jsxHeading);
---
+++ 03_LayingFoundation/notes.md
@@ -0,0 +1,11 @@
+## Q1: JSX is different from react
+jsx is not html in js its different 
+jsx like syntax of html or XML
+all about jsx 
+
+## js engine ecma script understand not jsx 
+browser not understand jsx
+then how working?
+parcel is doing job behind the scene code transpiled before reches to js engine so that it can easily understands by engine
+
+Parcel -> babbel transpiling our code takes jsx converts code that js engine understand
